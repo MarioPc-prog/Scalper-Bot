c22f5c83209b35b8488e4ad90712479a1d2744f2
Mario <mavega@uvm.edu>
2021-07-03 16:23:08 -0500

    Created an instance of the webdriver , Added path to the instance. Browser now pops up with nothing displayed and test case passed.

--- Scalper.py
+++ Scalper.py
@@ -1,2 +1,6 @@
 import time
 from selenium import webdriver
+
+#For using Chrome , create an instance of Chrome WebDrier
+Browser = webdriver.Chrome("/Users/maveg/OneDrive/Desktop/chromedriver")
+print("Test")
