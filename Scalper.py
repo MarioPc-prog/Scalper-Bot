746edee5a11528a53470cc4b4e93b31c152d4979
Mario <mavega@uvm.edu>
2021-07-03 16:28:34 -0500

    Linked method call Browser.get() to the best buy ps5 digital url link , test passed however the page doesnt load as quickly as I thought , Maybe future improvement can come into faster loading screen url.

--- Scalper.py
+++ Scalper.py
@@ -3,4 +3,8 @@ from selenium import webdriver
 
 #For using Chrome , create an instance of Chrome WebDrier
 Browser = webdriver.Chrome("/Users/maveg/OneDrive/Desktop/chromedriver")
+
+#Call Browser.get method so you can navigate to a page given by the url .
+Browser.get("https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161")
+
 print("Test")
