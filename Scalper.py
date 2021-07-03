ed8c78bbf2c300f4ff0b6b177d64386d8d65212e
Mario <mavega@uvm.edu>
2021-07-03 17:42:33 -0500

    Added a boolean flag called add buyButton to handle a while loop while false , implemented an except also both try and except are using the Browser find method .WebDriver offers a number of ways to find elements using one of the find_element_by_* methods . Later I will give a brief break down once i review try and except .

--- Scalper.py
+++ Scalper.py
@@ -7,4 +7,16 @@ Browser = webdriver.Chrome("/Users/maveg/OneDrive/Desktop/chromedriver")
 #Call Browser.get method so you can navigate to a page given by the url .
 Browser.get("https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161")
 
-print("Test")
+
+#Boolean flag
+buyButton = False
+
+while not buyButton:
+    try:
+        #WebDriver offers a number of ways to find elements using one of the find_element_by_* methods
+        addToCartBtn = addButton = Browser.find_element_by_class_name("btn-primary")
+        print("Test in try")
+    except:
+        addToCartBtn = addButton = Browser.find_element_by_class_name("btn-primary")
+        print("Test in except")
+
