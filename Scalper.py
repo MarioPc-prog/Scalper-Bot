336e209980f23a003a6dcd7b97235a65fe877f90
Mario <mavega@uvm.edu>
2021-07-04 11:22:53 -0500

    up to date Next commit - Go to cart button functionality

--- Scalper.py
+++ Scalper.py
@@ -1,16 +1,12 @@
 import time
 from selenium import webdriver
 
-#For using Chrome , create an instance of Chrome WebDrier
+# For #using Chrome , create an instance of Chrome WebDrier
 Browser = webdriver.Chrome("/Users/maveg/OneDrive/Desktop/chromedriver")
-
-#Call Browser.get method so you can navigate to a page given by the url .
+# Call Browser.get method too navigate to a page given by the url
 Browser.get("https://www.bestbuy.com/site/lego-classic-medium-creative-brick-box-building-set-10696/6252643.p?skuId=6252643&ref=212&loc=1&extStoreId=1502&ref=212&loc=BM01&gclid=CjwKCAjwlYCHBhAQEiwA4K21mwp9SMkEeEfxwyR_S0QyzWU4ZsWnMV57XeN6gKzZT2pG-Y9sf3oAFBoCQrcQAvD_BwE&gclsrc=aw.ds")
-
-
-#Boolean flag
+# Boolean flag
 buyButton = False
-
 while not buyButton:
     try:
         #WebDriver offers a number of ways to find elements using one of the find_element_by_* methods
@@ -19,7 +15,6 @@ while not buyButton:
         #Refresh page after a delay
         time.sleep(3)
         Browser.refresh()
-
     except:
         addToCartBtn = addButton = Browser.find_element_by_class_name("btn-primary")
         print("Button was Clicked")
