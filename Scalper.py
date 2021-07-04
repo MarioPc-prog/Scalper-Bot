3cada4dd3ffed07feb306be05058c6d3c73da552
Mario <mavega@uvm.edu>
2021-07-03 19:55:11 -0500

    Got the class method find working . Succesfully the product is added to the cart Beta test phase A working thus far . Next phase is to figure out how to use the go to cart option next

--- Scalper.py
+++ Scalper.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 Browser = webdriver.Chrome("/Users/maveg/OneDrive/Desktop/chromedriver")
 
 #Call Browser.get method so you can navigate to a page given by the url .
-Browser.get("https://www.bestbuy.com/site/sony-playstation-5-digital-edition-console/6430161.p?skuId=6430161")
+Browser.get("https://www.bestbuy.com/site/lego-classic-medium-creative-brick-box-building-set-10696/6252643.p?skuId=6252643&ref=212&loc=1&extStoreId=1502&ref=212&loc=BM01&gclid=CjwKCAjwlYCHBhAQEiwA4K21mwp9SMkEeEfxwyR_S0QyzWU4ZsWnMV57XeN6gKzZT2pG-Y9sf3oAFBoCQrcQAvD_BwE&gclsrc=aw.ds")
 
 
 #Boolean flag
@@ -14,9 +14,15 @@ buyButton = False
 while not buyButton:
     try:
         #WebDriver offers a number of ways to find elements using one of the find_element_by_* methods
-        addToCartBtn = addButton = Browser.find_element_by_class_name("btn-primary")
-        print("Test in try")
+        addToCartBtn = addButton = Browser.find_element_by_class_name("btn-disabled")
+        print("Button isn,t ready yet.")
+        #Refresh page after a delay
+        time.sleep(3)
+        Browser.refresh()
+
     except:
         addToCartBtn = addButton = Browser.find_element_by_class_name("btn-primary")
-        print("Test in except")
+        print("Button was Clicked")
+        addToCartBtn.click()
+        buyButton = True
 
